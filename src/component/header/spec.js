f7c42ed121fee50de3d48a34f424442bbb4c34a4
Manish Mishra <manishm@simplifyworkforce.com>
2019-04-30 09:42:04 +0530

    adding files

---
+++ src/component/header/spec.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { shallow } from 'enzyme';
+import Header from './index';
+describe('Header Component', () => {
+
+    it('It should render without errors', () => {
+        expect(1).toBe(3);
+    })
+})
